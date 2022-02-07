1065f49735395742e41fb7229419b1861e4bf889
Ümit <47325422+UemitGuel@users.noreply.github.com>
2022-02-07 12:03:33 +0100

    style detail page

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,15 +1,68 @@
-import { Text } from "@chakra-ui/react";
+import {
+    Container,
+    SimpleGrid,
+    Image,
+    Flex,
+    Heading,
+    Text,
+    Stack,
+    StackDivider,
+    Icon,
+    AspectRatio,
+  } from '@chakra-ui/react';
+import {
+    IoAnalyticsSharp,
+    IoLogoBitcoin,
+    IoSearchSharp,
+  } from 'react-icons/io5';
+import { ReactElement } from 'react';
 import { getCoffeePlaceIDs, getPlaceByID } from "../../lib/airtable";
-import React from "react";
 import { chakra, useColorModeValue } from "@chakra-ui/react";
+import Head from 'next/head';
+import FeatureView from '../../components/featureView';
 
 const DetailView = ({place}) => {
     const fields = place.place[0].fields
 
     return (
-        <Text>{fields.name}</Text>
-    )
+          <Container maxW={'5xl'} py={12}>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              <Stack spacing={4}>
+                <FeatureView 
+                    features={fields.features} 
+                />
+                {fields.noteHeadline != null ? <Heading>{fields.name}</Heading> : <Heading>{fields.name} - {fields.noteHeadline}</Heading>}
+                {fields.noteHeadline && 
+                    <Text color={'gray.500'} fontSize={'lg'}>
+                        {fields.note}
+                    </Text> 
+                }
+                <Stack
+                  spacing={4}
+                  divider={
+                    <StackDivider
+                      borderColor={useColorModeValue('gray.100', 'gray.700')}
+                    />
+                  }>
+          </Stack>
+        </Stack>
+        {fields.image[0].url &&
+        <AspectRatio>
+            <Flex>
+                <Image
+                    rounded={'md'}
+                    alt={'feature image'}
+                    src={fields.image[0].url}
+                    objectFit={'cover'}
+                />
+            </Flex>
+        </AspectRatio>
+        }
+      </SimpleGrid>
+    </Container>
+  );
 };
+
 export async function getStaticPaths() {
     const paths = await getCoffeePlaceIDs()
     return {
