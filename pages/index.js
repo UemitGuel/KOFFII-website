b0f11b565b5f71a92f32abd15dfa2dacdfd66edb
Ümit <47325422+UemitGuel@users.noreply.github.com>
2022-02-12 13:07:57 +0100

    connected knowledge database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { getCoffeePlaces, getPlacesInSpotlight } from '../lib/airtable';
+import { getCoffeePlaces, getKnowledge, getPlacesInSpotlight } from '../lib/airtable';
 import { SimpleGrid, Container, Heading, Text, Divider,Flex, IconButton } from '@chakra-ui/react';
 import InSpotlightCard from "../components/inSpotlightCard";
 import {React, useState} from "react";
@@ -10,10 +10,12 @@ import { FiMenu } from 'react-icons/fi'
 export async function getStaticProps() {
   const coffeeData = await getCoffeePlaces()
   const spotlightData = await getPlacesInSpotlight()
+  const knowledgeData = await getKnowledge()
   return {
     props: {
       coffeeData: coffeeData,
-      spotlightData: spotlightData
+      spotlightData: spotlightData,
+      knowledgeData: knowledgeData
     }
   }
 }
@@ -35,44 +37,54 @@ export default function Home(props) {
 
   const spotlightData = props.spotlightData
   const coffeeData = props.coffeeData
+  const knowledgeData = props.knowledgeData
+  console.log(knowledgeData)
 
   return (
     <Flex w="100%">
       <Sidebar activeTabs={activeTabs} toggleTab={toggleTab}/>
       {activeTabs.firstTab &&
-    <Container maxW='container.xl' py='20'>
-      <Heading>Meine Lieblingscafes</Heading>
-      <SimpleGrid minChildWidth='250px' spacing={8} py={8}>
-      {spotlightData.map((spotlightData) => (
-        <Link href={`/detail/${spotlightData.id}`} unstyled>
-            <InSpotlightCard 
-            key={spotlightData.id} 
-            name={spotlightData.fields.name} 
-            hood={spotlightData.fields.hood} 
-            note={spotlightData.fields.note}
-            noteHeadline={spotlightData.fields.noteHeadline}
-            src={spotlightData.fields.image[0].url}
-            features={spotlightData.fields.features}
-            /> 
-        </Link>
-        ))}
-      </SimpleGrid>
-      <Divider />
-      <Heading>Weitere Cafes</Heading>
-      <SimpleGrid minChildWidth='250px' spacing={8} py={8}>
-      {coffeeData.map((coffeeData) => (
-        <Link href={`/detail/${spotlightData.id}`} unstyled>
-          <CoffeeCard 
-            key={coffeeData.id} 
-            name={coffeeData.fields.name} 
-            hood={coffeeData.fields.hood} 
-            features={coffeeData.fields.features}
-          />
-        </Link>
-      ))}
-      </SimpleGrid>
-    </Container>
-    }
+        <Container maxW='container.xl' py='20'>
+          <Heading>Meine Lieblingscafes</Heading>
+          <SimpleGrid minChildWidth='250px' spacing={8} py={8}>
+          {spotlightData.map((spotlightData) => (
+            <Link href={`/detail/${spotlightData.id}`} unstyled>
+                <InSpotlightCard 
+                key={spotlightData.id} 
+                name={spotlightData.fields.name} 
+                hood={spotlightData.fields.hood} 
+                note={spotlightData.fields.note}
+                noteHeadline={spotlightData.fields.noteHeadline}
+                src={spotlightData.fields.image[0].url}
+                features={spotlightData.fields.features}
+                /> 
+            </Link>
+            ))}
+          </SimpleGrid>
+          <Divider />
+          <Heading>Weitere Cafes</Heading>
+          <SimpleGrid minChildWidth='250px' spacing={8} py={8}>
+          {coffeeData.map((coffeeData) => (
+            <Link href={`/detail/${spotlightData.id}`} unstyled>
+              <CoffeeCard 
+                key={coffeeData.id} 
+                name={coffeeData.fields.name} 
+                hood={coffeeData.fields.hood} 
+                features={coffeeData.fields.features}
+              />
+            </Link>
+          ))}
+          </SimpleGrid>
+        </Container>
+      }
+      {activeTabs.secondTab &&
+        <Container maxW='container.xl' py='20'>
+          <Heading>Information& Zubereitung</Heading>
+          {knowledgeData.map((knowledgeData) => (
+            <Text>{knowledgeData.fields.title}</Text>   
+           ))}
+        </Container>
+      }
     </Flex>
     
   )
