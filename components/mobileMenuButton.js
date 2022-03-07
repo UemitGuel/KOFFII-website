6352661ef2ffb88412e0135e3fae142d6a64cab2
Ümit <47325422+UemitGuel@users.noreply.github.com>
2022-03-07 10:35:34 +0100

    Mobile Navigation

--- components/mobileMenuButton.js
+++ components/mobileMenuButton.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { VStack, Text, useColorModeValue, Icon } from "@chakra-ui/react";
+import { Button, Text, useColorModeValue, Icon } from "@chakra-ui/react";
 
 const MobileMenuButton = ({ label, icon, ...rest }) => {
   return (
-    <VStack
-      as="button"
-      spacing={0}
-      rounded="md"
-      px={6}
-      {...rest}
-    >
-      <Icon>{icon}</Icon>
-
-      <Text
-        fontSize="xs"
-        fontWeight="500"
-      >
-        {label}
-      </Text>
-    </VStack>
+    <Button leftIcon={<MdBuild />} colorScheme='pink' variant='solid'>
+    Settings
+  </Button>
   );
 };
 
--- components/mobileNavigation.js
+++ components/mobileNavigation.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {
   Box,
-  Center,
+  Spacer,
   HStack,
   IconButton,
   useColorModeValue,
   Button,
   VStack,
 } from "@chakra-ui/react";
-import { MdWifi, MdLocalDining} from "react-icons/md";
+import { MdWifi, MdLocalDining } from "react-icons/md";
 import { motion, useCycle } from "framer-motion";
 import MobileMenuButton from "./mobileMenuButton";
 import Link from "next/link";
+import ThemeToggle from "./themeToggle";
 
 const MobileNavigation = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
@@ -55,6 +56,7 @@ const MobileNavigation = () => {
       right="0"
       left="0"
       display={{ base: "block", md: "none" }}
+      backgroundColor={useColorModeValue("neutral.100", "neutralD.100")}
     >
       <HStack
         justify="space-around"
@@ -64,23 +66,16 @@ const MobileNavigation = () => {
         height={16}
         borderTopWidth="2px"
         shadow="0 -2px 10px 0 rgba(0,0,0, 0.035);"
+        px={4}
       >
-        <Center w="100%">
-          <Link href="/blog" px={6}>
-            <MobileMenuButton
-              label="Blog"
-              icon={<MdWifi />}
-            ></MobileMenuButton>
+          <Link href="/" px={6}>
+            <Button variant="outline">Cafés</Button>
           </Link>
-        </Center>
-        <Center w="100%">
-          <Link href="/newsletter" px={6}>
-            <MobileMenuButton
-              label="Newsletter"
-              icon={<MdLocalDining />}
-            ></MobileMenuButton>
+          <Link href="/preparation" px={6}>
+            <Button variant="outline">Zubereitung& mehr</Button>
           </Link>
-        </Center>
+          <Spacer/>
+        <ThemeToggle/>
       </HStack>
     </MotionBox>
   );
