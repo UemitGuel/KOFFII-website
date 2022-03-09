60ded675e0916c3136d4b71a41f2e9e19d596176
Ümit <47325422+UemitGuel@users.noreply.github.com>
2022-03-09 16:27:40 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,10 @@ import theme from "../theme";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <head>
+        <title>Title</title>
+        <meta property="og:image" content="https://images.unsplash.com/photo-1640622307985-eaa37b36077f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+      </head>
       <Header />
       <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
         <Component {...pageProps} />
