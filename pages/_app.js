26e8231b0a526948fc74846f83416c33638d70fb
Ümit <47325422+UemitGuel@users.noreply.github.com>
2022-03-09 16:34:02 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,16 @@ import MobileNavigation from "../components/mobileNavigation";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <head>
-        <title>Title</title>
-        <meta property="og:image" content="https://images.unsplash.com/photo-1640622307985-eaa37b36077f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
-      </head>
+      <Head>
+        <title>This page has a title 🤔</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
         <Component {...pageProps} />
